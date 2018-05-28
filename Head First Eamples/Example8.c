2dfa3c11c50a1a47903a52df1c2b4b62192d2d3a
Rahul Charan <rahulcharan0190@outlook.com>
2018-05-28 08:25:17 +0530

    Simple Black Jack Based Example

---
+++ Head First Eamples/Example8.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+char card_name[3];
+puts("Please enter the card name:");
+scanf("%2s",card_name);
+int val=0;
+if(card_name[0]=='K'){
+    val =10;
+}
+else if(card_name[0]=='Q'){
+    val=10;
+}
+else if(card_name[0]=='J'){
+    val=10;
+}
+else if(card_name[0]=='A'){
+    val=11;
+}
+else{
+   val=atoi(card_name);
+}
+if(val>2 && val<7){
+    puts("Count has gone up");
+}
+else if(val==10){
+    puts("count has gone down");
+}
+
+return 0;
+}
