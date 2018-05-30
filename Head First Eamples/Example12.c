bd2af1b4951518b2ddab7143122929135fd2f718
Rahul Charan <rahulcharan0190@outlook.com>
2018-05-30 07:03:40 +0530

    Simple Adress and value Program

--- Head First Eamples/Example12.c
+++ Head First Eamples/Example12.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+void goSouthEast(int *lat,int *lon){
+    *lat=*lat-1;
+    *lon=*lon+1;
+}
+
+int main(){
+int latitude,longitude;
+latitude=32;
+longitude=10;
+printf("Initial latitude is %d\n",latitude);
+printf("Initial longitude is %d\n",longitude);
+goSouthEast(&latitude,&longitude);
+printf("Ship is moving\n");
+printf("New latitude is %d\n",latitude);
+printf("New longitude is %d",longitude);
+
+}
 
